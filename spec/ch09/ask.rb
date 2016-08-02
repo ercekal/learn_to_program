52ecbeb3f5f7e100927ac7658450d4740b542aed
Erce Kalabalikoglu <ercekalabalikoglu@gmail.com>
2016-08-02 18:50:34 +0100

    Add files via upload

---
+++ spec/ch09/ask.rb
@@ -0,0 +1,18 @@
+def ask question
+  while true
+    puts question
+    reply = gets.chomp.downcase
+
+      if reply == 'yes'
+        return true
+      end
+      if reply == 'no'
+        return false
+      end
+else
+      puts 'Please answer Yes & No'
+  end
+end
+
+likes_it = ask 'Do you like eating tacos?'
+puts likes_it
